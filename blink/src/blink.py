5cdea21d35e7f787cbb7bb26b908ad1317999525
Michael Allen <michael@allenhub.net>
2018-05-02 16:06:13 +0000

    use button to stop/start blinking

--- blink/src/blink.py
+++ blink/src/blink.py
@@ -15,24 +15,31 @@ def flicker(ledPin, finalState = GPIO.LOW):
     
 
 def button_press(channel):
-    if(GPIO.input(buttonPin) == GPIO.input(ledPin)):
-        flicker(ledPin)
-    GPIO.output(ledPin, GPIO.input(buttonPin))
+    oldLed = GPIO.input(ledPin)
+#    if(GPIO.input(buttonPin) == GPIO.input(ledPin)):
+#        flicker(ledPin)
+    if(oldLed):
+        GPIO.output(ledPin, GPIO.LOW)
+    else:
+        GPIO.output(ledPin, GPIO.HIGH)
 
 
-
-def blink(pin, delay, cycles = 0):
+def blink(pin, delay, enablePin = 0, cycles = 0):
     if cycles:
         for x in range(0,cycles):
-            GPIO.output(pin, GPIO.HIGH)
+            if(GPIO.input(enablePin)):
+                GPIO.output(pin, GPIO.HIGH)
             time.sleep(delay)
-            GPIO.output(pin, GPIO.LOW)
+            if(GPIO.input(enablePin)):
+                GPIO.output(pin, GPIO.LOW)
             time.sleep(delay)
     else:
         while True:
-            GPIO.output(pin, GPIO.HIGH)
+            if(GPIO.input(enablePin)):
+                GPIO.output(pin, GPIO.HIGH)
             time.sleep(delay)
-            GPIO.output(pin, GPIO.LOW)
+            if(GPIO.input(enablePin)):
+                GPIO.output(pin, GPIO.LOW)
             time.sleep(delay)
             
 
@@ -52,22 +59,22 @@ GPIO.setup(   ledPin, GPIO.OUT, initial=GPIO.HIGH         )
 GPIO.setup(buttonPin,  GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 
-#GPIO.add_event_detect(buttonPin, GPIO.BOTH, bouncetime=20)
-GPIO.add_event_detect(buttonPin, GPIO.BOTH)
-GPIO.add_event_callback(buttonPin, button_press)
+GPIO.add_event_detect(buttonPin, GPIO.RISING, button_press, bouncetime=300)
+#GPIO.add_event_detect(buttonPin, GPIO.FALLING, callback=call_on_falling)
+#GPIO.add_event_callback(buttonPin, 
 
 
 
 
-blueThread = Thread(target = blink, args=(7, 0.1))
+blueThread = Thread(target = blink, args=(7, 0.1, ledPin))
 blueThread.daemon=True
 blueThread.start()
 
-yellowThread = Thread(target = blink, args=(11, 2))
+yellowThread = Thread(target = blink, args=(11, 2, ledPin))
 yellowThread.daemon = True
 yellowThread.start()
 
-redThread = Thread(target = blink, args=(13, 1))
+redThread = Thread(target = blink, args=(13, 1, ledPin))
 redThread.daemon = True
 redThread.start()
 
