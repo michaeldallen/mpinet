82639b5a9c06c7aeb0a31595d6ef34b90a76507d
Michael Allen <mallen@mallen-deb9-64>
2018-12-03 19:27:02 -0500

    start building ansible control node container

--- bootstrap.sh
+++
@@ -1,5 +0,0 @@
-sudo apt-get install --assume-yes dirmngr software-properties-common
-echo 'deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main' |  sudo tee /etc/apt/sources.list.d/ansible.list
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
-sudo apt-get update
-sudo apt-get install --assume-yes ansible
